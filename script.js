b13014df1eb5ee8a2427bcccc96357b4badd8ab2
webnugroho1 <web.nugroho1@gmail.com>
2025-08-03 01:56:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+window.addEventListener("DOMContentLoaded", () => {
+  fetch('partials/header.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    });
 
+  fetch('partials/nav.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('nav').innerHTML = data;
+
+      // Setelah navigasi dimuat, aktifkan toggle menu
+      const toggle = document.querySelector('.menu-toggle');
+      if (toggle) {
+        toggle.addEventListener('click', () => {
+          const navLinks = document.getElementById('navLinks');
+          navLinks.classList.toggle('active');
+        });
+      }
+    });
+});
